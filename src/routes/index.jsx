099e11666642e71ee37a9f42de013fc89b5ad80c
HammedforJesus <oreoluwahammed09@gmail.com>
2026-04-02 07:49:19 +0100

    new update

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -47,7 +47,7 @@ function RouteComponent() {
                 filteredCourses.length ?
                     <section className='flex-1 flex flex-col items-center gap-4 overflow-y-auto py-4'>
                         {filteredCourses.map((c, i) => (
-                            <ClassCard key={i} level={`${c.level} LEVEL`} courseCode={c.courseCode} startTime={c.dayAndTime[0].startTime} endTime={c.dayAndTime[0].endTime} active={c.dayAndTime[0].classActive} />
+                            <ClassCard key={i} {...c} />
                         ))}
                     </section>
                     :
